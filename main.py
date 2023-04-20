92f40e2d52c1150364f3c9bcdfd3cd27f031f1c3
Gabriel Moreira C <128189786+gmoreirac26@users.noreply.github.com>
2023-04-20 15:51:26 -0300

    tentei arrumar
    Signed-off-by: Gabriel Moreira C <128189786+gmoreirac26@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if (metros_quadrados%54 != 0):
     qtd_de_latas = qtd_de_latas +1 
 valor_total = qtd_de_latas * 80 
 
-print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total} .") 
+print(f"Serão necessárias {int(qtd_de_latas)} latas totalizando R$ {int(valor_total)} .") 
